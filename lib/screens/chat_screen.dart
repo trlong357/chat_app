8139779f444a444c85a70ee4ddab58714f1943d5
Long Tran <longtran@192.168.2.21>
2022-08-05 17:45:13 +0700

    getting started with firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -7,13 +8,29 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              padding: const EdgeInsets.all(8),
-              child: Text('abc'),
-            );
-          }),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: Text('abc'),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection(
+                "chats/ESW4Hpp5tGKGXrcd1qRN/messages",
+              )
+              .snapshots()
+              .listen((event) {
+            event.docs.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
